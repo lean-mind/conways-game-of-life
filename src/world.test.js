476fa532bdf0170597fa7e6b308e6fe924edd4b8
Daniel Ramos <danielramosacosta@hotmail.com>
2018-07-05 16:42:12 +0100

    Green: "creates an empty world"

--- src/world.test.js
+++ src/world.test.js
@@ -1,7 +1,28 @@
+class World {
+  constructor () {
+  }
+
+  getSnapshot () {
+    return [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
+  }
+}
+
 describe("the world behavior", () => {
   describe("the creation", () => {
+    let world;
+    beforeEach(() => {
+      world = new World()
+    })
     it("creates an empty world", () => {
-      expect(true).toBe(false)
+      expect(world.getSnapshot()).toEqual([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ])
     })
   })
 })
