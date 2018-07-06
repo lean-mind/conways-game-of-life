5b075af87d63ac9d4edbf111bbd424acb75e4e0f
Daniel Ramos <danielramosacosta@hotmail.com>
2018-07-06 11:31:22 +0100

    TODO

--- src/world.test.js
+++ src/world.test.js
@@ -1,28 +1,58 @@
 class World {
-  constructor () {
+  constructor (ejeAbscisas, ejeOrdenadas) {
+    this.ejeAbscisas
+    this.ejeOrdenadas
+
+    this.world = this.createWorld(ejeAbscisas, ejeOrdenadas)
+  }
+
+  createWorld(ejeAbscisas, ejeOrdenadas) {
+    return new Array(ejeAbscisas).fill(Array(ejeOrdenadas).fill(null))
+  }
+
+  filler () {
+    this.world = [
+      [new Cell(), new Cell()],
+      [new Cell(), new Cell()]
+    ]
   }
 
   getSnapshot () {
-    return [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
+    return this.world
   }
+}
+
+class Cell {
+  constructor () {}
 }
 
 describe("the world behavior", () => {
   describe("the creation", () => {
-    let world;
-    beforeEach(() => {
-      world = new World()
-    })
-    it("creates an empty world", () => {
+    it("creates an empty world of the given size", () => {
+      const world = new World(4, 4)
+      world1 = world
       expect(world.getSnapshot()).toEqual([
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
       ])
+    })
+    it("creates a world with cells", () => {
+      const world = new World(4, 4)
+      world.filler()
+      const snapshot = world.getSnapshot()
+      expect(snapshot).toHaveLength(4)
+      snapshot.forEach(row => {
+        expect(snapshot).toHaveLength(4)
+        row.forEach(cell => {
+          expect(cell).toBeInstanceOf(Cell)
+        })
+      })
+      /*expect().toEqual([
+        [null, null],
+        [null, null]
+      ])*/
     })
   })
 })
