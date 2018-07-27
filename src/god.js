903a3ad99a7aba0d9ecec14c5047431d0cc15839
Rosana Ruiz <rosanarufer@gmail.com>
2018-07-27 13:16:10 +0100

    Green: God creates a world

--- src/god.js
+++ src/god.js
@@ -1,9 +1,20 @@
+const { Cell } = require("./cell");
+
 class God {
   constructor() {}
 
   createWorldWithAliveCells(...cells) {
     cells[0].kill();
     return {};
+  }
+
+  createWorld() {
+    return [
+      [new Cell(), new Cell(), new Cell(), new Cell()],
+      [new Cell(), new Cell(), new Cell(), new Cell()],
+      [new Cell(), new Cell(), new Cell(), new Cell()],
+      [new Cell(), new Cell(), new Cell(), new Cell()]
+    ]
   }
 
   mutateGeneration(world) {}
