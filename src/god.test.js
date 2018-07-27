ff276f916a59c663192ca34f0eaac6edc0f2be40
Rosana Ruiz <rosanarufer@gmail.com>
2018-07-27 13:15:52 +0100

    Red: god creates a world

--- src/god.test.js
+++ src/god.test.js
@@ -8,13 +8,28 @@ describe("the god behavior", () => {
     expect(aCell.isAlive()).toBeTruthy();
     const world = god.createWorldWithAliveCells(
       aCell,
-      aCell.mitosis(),
-      aCell.mitosis(),
-      aCell.mitosis()
+      new Cell(),
+      new Cell(),
+      new Cell()
     );
 
     god.mutateGeneration(world);
 
     expect(aCell.isAlive()).toBeFalsy();
   });
+
+  it('creates a world', function () {
+    // Arrange
+    const god = new God();
+    const world =god.createWorld();
+
+    // Expect
+    expect(world.length).toEqual(4);
+    expect(world[0].length).toEqual(4);
+    expect(world[1].length).toEqual(4);
+    expect(world[2].length).toEqual(4);
+    expect(world[3].length).toEqual(4);
+
+  });
+
 });
