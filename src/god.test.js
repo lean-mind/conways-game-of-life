3062531f04526ae033b1dff2acda482827b1bad4
rubentejera <ruben.tejera@leanmind.es>
2018-07-27 13:26:08 +0100

    red: each cell with one or no neighbors dies, as if by solitude.

--- src/god.test.js
+++ src/god.test.js
@@ -3,30 +3,44 @@ const { Cell } = require("./cell");
 
 describe("the god behavior", () => {
 
-  it("each cell with one or no neighbors dies, as if by solitude.", () => {
+  describe("the cell behavior on the world", () => {
+    let god;
+    let aCell;
 
+    beforeEach(() => {
+      god = new God();
+      aCell = Cell.born();
+      expect(aCell.isAlive()).toBeTruthy();
+    });
+
+    it("each cell with one or no neighbors dies, as if by solitude.", () => {
+      const world = god.createWorldWithAliveCells(
+        aCell,
+        aCell.mitosis()
+      );
+
+      expect(aCell.isAlive()).toBeTruthy();
+    });
+
+    it("each cell with four or more neighbors dies, as if by overpopulation.", () => {
+      const world = god.createWorldWithAliveCells(
+        aCell,
+        aCell.mitosis(),
+        aCell.mitosis(),
+        aCell.mitosis(),
+        aCell.mitosis()
+      );
+
+      god.mutateGeneration(world);
+
+      expect(aCell.isAlive()).toBeFalsy();
+    });
+
+    it("each cell with two or three neighbors survives.", () => {
+
+    });
   });
 
-  it("each cell with four or more neighbors dies, as if by overpopulation.", () => {
-    const god = new God();
-    const aCell = Cell.born();
-    expect(aCell.isAlive()).toBeTruthy();
-    const world = god.createWorldWithAliveCells(
-      aCell,
-      aCell.mitosis(),
-      aCell.mitosis(),
-      aCell.mitosis(),
-      aCell.mitosis()
 
-    );
-
-    god.mutateGeneration(world);
-
-    expect(aCell.isAlive()).toBeFalsy();
-  });
-
-  it("each cell with two or three neighbors survives.", () => {
-
-  });
 
 });
